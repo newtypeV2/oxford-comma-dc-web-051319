03e7d13ec73e916376c366d012abc41e41a63603
Gian Libunao <gianlibunao@gmail.com>
2019-04-26 18:30:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+printString = ""
+if array.length ==1
+  printString << array.first
+elsif array.length==2
+  printString << "#{array.first} and #{array.last}"
+else
+array.each do |x|
+if array.last==x
+  printString << " and #{x}"
+elsif array.first==x
+  printString << "#{x},"
+else
+  printString << " #{x},"
+end
+end
+end
+printString
 end
